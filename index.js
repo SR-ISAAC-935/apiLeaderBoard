9f1c7989c517e47f28e736a3ecdf99b8ee4ff78c
sergio guerra <isaac7Guerra@gmail.com>
2026-04-20 19:01:36 -0600

    Guardando cambios antes del merge

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express'); 
+const cors = require('cors'); // 👈 IMPORTANTE
 const app = express();
 
-// 1. Importaciones de rutas
+// 🔥 CONFIGURACIÓN CORS
+app.use(cors({
+    origin: '*', // permite todos (dev)
+}));
+
+app.use(express.json()); 
+
+// Rutas
 const auth = require('./routes/auth');
 const responses = require('./routes/responses');
 const leaderboard = require('./routes/leaderboardsRoutes');
 const { connectSQLServer } = require('./DB/databaseConfig');
-// 2. Middlewares de configuración (SIEMPRE ANTES DE LAS RUTAS)
-app.use(express.json()); 
 
-// 3. Definición de rutas
 app.use('/auth', auth);
 app.use('/', responses);
 app.use('/leaderboard', leaderboard);
-app.use(connectSQLServer); // Middleware para conectar a la base de datos
-// 4. Encendido del servidor
+
+// ⚠️ ESTE NO VA COMO MIDDLEWARE
+// app.use(connectSQLServer); ❌
+
+// Servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`🚀 Server is running on: http://localhost:${port}/`);
+    console.log(`🚀 Server running`);
 });
