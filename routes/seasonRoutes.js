0769ef31598fc5501d37f2fdcfa4702686b82b7b
sergio guerra <isaac7Guerra@gmail.com>
2026-04-25 15:43:00 -0600

    add routing seasons

--- routes/seasonRoutes.js
+++ routes/seasonRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { tokenTrueFalse } = require('../middleware/checkToken');
 const { createSeason, getSeasons} = require('../controllers/seasonControllers')
 
-router.get('/', tokenTrueFalse, getSeasons);
+router.get('/get-seasons', tokenTrueFalse, getSeasons);
 router.post('/create', tokenTrueFalse, createSeason);
 module.exports = router;
