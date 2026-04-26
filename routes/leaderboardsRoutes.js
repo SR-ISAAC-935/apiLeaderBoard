6274bfd49e68756e2e803b102ecdd820f7f7503f
sergio guerra <isaac7Guerra@gmail.com>
2026-04-25 18:23:20 -0600

    response teamscontroller

--- routes/leaderboardsRoutes.js
+++ routes/leaderboardsRoutes.js
@@ -5,7 +5,7 @@ const { tokenTrueFalse } = require('../middleware/checkToken');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const { createMatchPlayed, getTeams}= require('../controllers/teamsControllers')
-
+const {updateScores} = require('../controllers/ScoresControllers')
 router.get('/', tokenTrueFalse, (req, res) => {
     res.json({ message: `Welcome to the leaderboard, ${req.user}!` });
 });
@@ -17,4 +17,5 @@ router.post(
 );
 router.post('/create-matches', tokenTrueFalse, createMatchPlayed);
 router.get('/getTeams',tokenTrueFalse,getTeams)
+router.put('/update-scores', tokenTrueFalse, updateScores);
 module.exports = router;
