8dbe54d126b066e21027895ea3683118db36bf4c
sergio guerra <isaac7Guerra@gmail.com>
2026-04-26 14:51:23 -0600

    agregando sockets para overlay

--- routes/leaderboardsRoutes.js
+++ routes/leaderboardsRoutes.js
@@ -5,7 +5,7 @@ const { tokenTrueFalse } = require('../middleware/checkToken');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const { createMatchPlayed, getTeams } = require('../controllers/teamsControllers');
-const { updateScores, getPositions } = require('../controllers/ScoresControllers'); // ✅ un solo import
+const { updateScores, getPositions, getPositionsAcumulated } = require('../controllers/ScoresControllers'); // ✅ un solo import
 
 router.get('/', tokenTrueFalse, (req, res) => {
     res.json({ message: `Welcome to the leaderboard, ${req.user}!` });
@@ -15,5 +15,5 @@ router.post('/create-matches', tokenTrueFalse, createMatchPlayed);
 router.get('/getTeams', tokenTrueFalse, getTeams);
 router.put('/update-scores', tokenTrueFalse, updateScores);
 router.get('/positions/:season_id', getPositions); // ✅ sin token
-
+router.get('/positions-acumulated', getPositionsAcumulated); // ✅ sin token
 module.exports = router;
